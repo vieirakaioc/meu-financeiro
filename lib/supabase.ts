60fd0388ba749bf75e96a61e2c4cff2dff851c43
Lucas Mendonça <seuemail@dominio.com>
2026-03-10 14:45:13 -0300

    ajuste

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://bfshqclxrfpqymcjqcxo.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmc2hxY2x4cmZwcXltY2pxY3hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjQ4NDgsImV4cCI6MjA4ODQwMDg0OH0.kISLqdos8vNzU5dcSIHVXD5wsamkAj9gXO4R2nb5KAQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
